45c59e5ef5344a9ccff057eb47c6cc915688b483
shivam02022002 <shivamgupta02022002@gmail.com>
2022-10-04 05:02:46 -0700

    fixed changedText file

--- labourService/src/components/ChangeText.tsx
+++ labourService/src/components/ChangeText.tsx
@@ -1,25 +1,28 @@
 import { useState, useCallback, useEffect } from "react";
-const names = ['Finding Labour Nearby you...', 'Fetching results...', 'Getting Information....']
+const names = [
+  "Finding Labour Nearby you...",
+  "Fetching results...",
+  "Getting Information....",
+];
 
 function ChangeText() {
-    const [newName, setnewName] = useState("");
-    const TextStyling = {
-        paddingTop: '20px',
-        paddingLeft: '10px'
-    };
-    const shuffle = useCallback(() => {
-        const index = Math.floor(Math.random() * names.length);
-        setnewName(names[index]);
-    }, []);
+  const [newName, setnewName] = useState("");
+  const TextStyling = {
+    paddingTop: "20px",
+    paddingLeft: "10px",
+  };
+  const shuffle = useCallback(() => {
+    const index = Math.floor(Math.random() * names.length);
+    setnewName(names[index]);
+  }, []);
 
-    useEffect(() => {
-        const intervalID = setInterval(shuffle, 500);
-        return () => clearInterval(intervalID);
-    }, [shuffle])
+  useEffect(() => {
+    const intervalID = setInterval(shuffle, 500);
+    return () => clearInterval(intervalID);
+  }, [shuffle]);
 
-    return (
-        <div style={TextStyling}>{newName}</div>
-    )
+  return (
+  <div style={TextStyling}>{newName}</div>
+  )
 }
-
 export default ChangeText;
