eacc91d24aaa2b98b9211c9d9857225a9609d268
shivam02022002 <shivamgupta02022002@gmail.com>
2022-10-06 20:48:12 +0530

    Added Database for login

--- labourService/src/pages/StartingPage.tsx
+++ labourService/src/pages/StartingPage.tsx
@@ -2,15 +2,70 @@ import {
   IonButton,
   IonContent,
   IonHeader,
+  IonInput,
   IonItem,
+  IonLabel,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./StartingPage.module.css";
 
-const StaringPage: React.FC = () => {
+const StartingPage: React.FC = () => {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    phone_no: "",
+    email: "",
+  });
+
+  let name, value;
+  const postUserData = (event: any) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUserData({ ...userData, [name]: value });
+  };
+
+  //connect with firebase
+  const submitData = async (e: any) => {
+    e.preventDefault();
+    const { firstName, lastName, phone_no, email } = userData;
+
+    if( firstName && lastName && phone_no && email ){
+
+    const res = await fetch(
+      "https://loginpage-5ac89-default-rtdb.firebaseio.com/userDataRecords.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          phone_no,
+          email,
+        }),
+      }
+    );
+    if (res) {
+      setUserData({
+        firstName: "",
+        lastName: "",
+        phone_no: "",
+        email: "",
+      });
+      alert("Data Stored");
+    } else {
+      alert("plz fill the data");
+    } 
+  } else {
+    alert("plz fill the data");
+  };
+};
   return (
     <IonPage>
       <IonHeader>
@@ -19,20 +74,66 @@ const StaringPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonItem className={classes.text1}>
-          <b>
-            Book Services like Electrician, plumber, Painter, Carpenter, Mason
-            at your fingure tips.
-          </b>
+        <IonItem className={classes.text1} lines="none">
+          <form method="POST">
+            <IonItem>
+              <IonLabel position="floating">First Name</IonLabel>
+              <IonInput
+                name="firstName"
+                type="text"
+                placeholder=""
+                clearInput={true}
+                value={userData.firstName}
+                inputMode="text"
+                onIonChange={postUserData}
+              ></IonInput>
+            </IonItem>
+
+            <IonItem>
+              <IonLabel position="floating">Last Name</IonLabel>
+              <IonInput
+                name="lastName"
+                type="text"
+                placeholder={""}
+                clearInput={true}
+                value={userData.lastName}
+                inputMode="text"
+                onIonChange={postUserData}
+              ></IonInput>
+            </IonItem>
+
+            <IonItem>
+              <IonLabel position="floating">Email</IonLabel>
+              <IonInput
+                name="email"
+                type="email"
+                placeholder={""}
+                clearInput={true}
+                value={userData.email}
+                inputMode="email"
+                onIonChange={postUserData}
+              ></IonInput>
+            </IonItem>
+
+            <IonItem>
+              <IonLabel position="floating">Phone No</IonLabel>
+              <IonInput
+                name="phone_no"
+                type="number"
+                clearInput={true}
+                value={userData.phone_no}
+                onIonChange={postUserData}
+              ></IonInput>
+            </IonItem>
+            <br />
+            <IonButton size="large" onClick={submitData}>
+              Submit
+            </IonButton>
+          </form>
         </IonItem>
-        <Link to={"/enter-phone-no"}>
-          <IonButton className={classes.ion_button_center} color="tertiary">
-            Continue with Phone no.
-          </IonButton>
-        </Link>
       </IonContent>
     </IonPage>
   );
 };
 
-export default StaringPage;
+export default StartingPage;
